108788f609549000b332f6a402247f0cd2fd2270
Eevika <84326942+Eeviika@users.noreply.github.com>
2024-02-23 15:04:37 -0500

    added more project settings; updated player and enemy objects; made testroom bigger; made ai smarter; changed usapi caching

--- scripts/ai_normal.gd
+++ scripts/ai_normal.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@onready var seePlayerCast: RayCast2D = $playerSightCast
-@onready var player : CharacterBody2D
+@onready var seeCast: RayCast2D = $playerSightCast
 @onready var navAgent : NavigationAgent2D = $nav
 @onready var pathfindTimer : Timer = Timer.new()
+@onready var wanderTimer : Timer = Timer.new()
+@onready var visibilityRange : Area2D = $visiblityRange
 @export var phase: GlobalEnums.EnemyPhase = GlobalEnums.EnemyPhase.Wander
 
 var pathfindTargetPos : Vector2 = Vector2(0,0)
@@ -15,29 +16,69 @@ func damage(damageAmount):
 	if health <= 0:
 		queue_free()
 
+func pathfind():
+	navAgent.target_position = pathfindTargetPos
+	nearPathfindTarget()
+	if phase == GlobalEnums.EnemyPhase.Chase and pathfindTargetPos == Vector2(-1,-1) and checkIfFacingPlayerObject():
+		print("finish chasing")
+		phase = GlobalEnums.EnemyPhase.Wander
+	if phase == GlobalEnums.EnemyPhase.Wander and pathfindTargetPos == Vector2(-1,-1):
+		var target = usapi.getRandomTilePosOnTilemap([Vector2i(0,0), Vector2i(1,0)].pick_random())
+		if !target: return;
+		wanderTimer.start()
+		pathfindTargetPos = target 
+
 func _ready():
 	pathfindTimer.wait_time = 0.1
 	pathfindTimer.autostart = true
 	pathfindTimer.one_shot = false
-	add_child(pathfindTimer)
 	pathfindTimer.timeout.connect(pathfind)
-	player = get_node_or_null('%player')
+	wanderTimer.wait_time = 5.5
+	wanderTimer.autostart = false
+	wanderTimer.one_shot = true
+	wanderTimer.timeout.connect(func()->void:
+		if checkIfFacingPlayerObject(): return;
+		pathfindTargetPos = Vector2(-1,-1)
+	)
+	add_child(pathfindTimer)
+	add_child(wanderTimer)
+	pathfindTargetPos = global_position
+
+func nearPathfindTarget():
+	if round(global_position.x + global_position.y) in [round(pathfindTargetPos.x + pathfindTargetPos.y), round(pathfindTargetPos.x + pathfindTargetPos.y)+1, round(pathfindTargetPos.x + pathfindTargetPos.y)-1, round(pathfindTargetPos.x + pathfindTargetPos.y)+2, round(pathfindTargetPos.x + pathfindTargetPos.y)-2]:
+		pathfindTargetPos = Vector2(-1,-1)
+
+func checkIfFacingPlayerObject() -> bool:
+	for i in visibilityRange.get_overlapping_bodies():
+		if !i.is_in_group("player"): continue;
+		return true;
+	return false;
 
 func _process(_delta):
-	pathfindTargetPos = get_global_mouse_position()
-	seePlayerCast.target_position = to_local(player.global_position if player else Vector2(0,0))
+	seeCast.target_position = Vector2(0,0)
+	for i in visibilityRange.get_overlapping_bodies():
+		if !i.is_in_group("player"): continue;
+		seeCast.target_position = to_local(i.global_position)
+		if seeCast.get_collider() == i: pathfindTargetPos = i.global_position; phase = GlobalEnums.EnemyPhase.Chase;
+
 
 func _physics_process(delta):
+	if pathfindTargetPos == Vector2(-1, -1): return;
 	var direction = Vector2.ZERO
 
 	direction = navAgent.get_next_path_position() - global_position
 	direction = direction.normalized()
 
-	look_at(pathfindTargetPos)
+	look_at(navAgent.get_next_path_position())
 
 	velocity = velocity.lerp(direction*speed,10*delta)
 
 	move_and_slide()
 
-func pathfind():
-	navAgent.target_position = pathfindTargetPos
+
+func _on_visiblity_range_body_entered(body:Node2D):
+	if !body.is_in_group("player"): return;
+
+
+func _on_visiblity_range_body_exited(body:Node2D):
+	if !body.is_in_group("player"): return;
--- scripts/usapi.gd
+++ scripts/usapi.gd
@@ -11,59 +11,101 @@ extends Node
 @export var projectSettingsDefaultPath = "uncoded_studios/"
 
 var objectCache = {}
+var audioCache = {}
+var resourceCache = {}
+var roomCache = {}
 var projectConfig = {}
 var modLoader = null
 
 func _ready():
-    print("Hello, World!")
-    projectConfig = {
-        "active": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/is_active"),
-        "objectsFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/objects_folder"),
-        "resourcesFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/resources_folder"),
-        "roomsFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/rooms_folder"),
-        "audioFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/audio_folder"),
-        "scriptsFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/scripts_folder"),
-    }
-    projectConfig.make_read_only() # Prevent any additional modifiers. Since we are grabbing from ProjectSettings, no reason to edit this anyway.
-    if projectConfig.active == false: push_warning("USAPI has been disabled in project config."); queue_free();
-    for i in projectConfig:
-        if i == "active": continue;
-        assert(projectConfig[i] != null, "ProjectSettings are not configured correctly. Missing \"{0}\".".format([i]))
-        assert(DirAccess.dir_exists_absolute(projectConfig[i]), "Directory for \"{0}\" is invalid; Points to \"{1}\"".format([i, projectConfig[i]]))
-    
-    if ResourceLoader.exists(projectConfig.scriptsFolder + "/usapi-modloader.gd"):
-        print("USAPI: Detected USAPI ModLoader!")
-        modLoader = load(projectConfig.scriptsFolder + "/usapi-modloader.gd")
-        var modLoaderNode := Node.new()
-        modLoaderNode.name = "modLoader"
-        add_child(modLoaderNode)
-        modLoaderNode.set_script(modLoader)
-        modLoader = modLoaderNode
+	print("Hello, World!")
+	projectConfig = {
+		"active": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/is_active"),
+
+		"objectsFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/objects_folder"),
+		"resourcesFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/resources_folder"),
+		"roomsFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/rooms_folder"),
+		"audioFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/audio_folder"),
+		"scriptsFolder": ProjectSettings.get_setting(projectSettingsDefaultPath + "usapi/scripts_folder"),
+
+		"cacheObjects": ProjectSettings.get_setting(projectSettingsDefaultPath + "caching/cache_all_objects_on_start"),
+		"cacheAudio": ProjectSettings.get_setting(projectSettingsDefaultPath + "caching/cache_all_audio_on_start"),
+		"cacheResources": ProjectSettings.get_setting(projectSettingsDefaultPath + "caching/cache_all_resources_on_start"),
+		"cacheRooms": ProjectSettings.get_setting(projectSettingsDefaultPath + "caching/cache_all_rooms_on_start"),
+		"cacheLimit": ProjectSettings.get_setting(projectSettingsDefaultPath + "caching/cache_limit"),
+	}
+	projectConfig.make_read_only() # Prevent any additional modifiers. Since we are grabbing from ProjectSettings, no reason to edit this anyway.
+	if projectConfig.active == false: print("USAPI has been disabled in project config."); queue_free();
+
+	for i in projectConfig:
+		if i == "active": continue;
+		assert(projectConfig[i] != null, "ProjectSettings are not configured correctly. Missing \"{0}\".".format([i]))
+		assert(DirAccess.dir_exists_absolute(projectConfig[i]), "Directory for \"{0}\" is invalid; Points to \"{1}\"".format([i, projectConfig[i]]))
+	
+	if ResourceLoader.exists(projectConfig.scriptsFolder + "/usapi-modloader.gd"):
+		print("USAPI: Detected USAPI ModLoader!")
+		modLoader = load(projectConfig.scriptsFolder + "/usapi-modloader.gd")
+		var modLoaderNode := Node.new()
+		modLoaderNode.name = "modLoader"
+		add_child(modLoaderNode)
+		modLoaderNode.set_script(modLoader)
+		modLoader = modLoaderNode
 
 ## Reloads the currently active scene. If hardReload is active, caches will be cleared as well.
 func reloadScene(hardReload=false):
-    if hardReload:
-        clearCache()
-    get_tree().reload_current_scene()
+	if hardReload:
+		clearCaches("object")
+		clearCaches("room")
+	get_tree().reload_current_scene()
 
 ## Summons an object into the current scene and returns it. If the object is not already cached, it will be cached to prevent lag. 
 ## If `cache` is disabled, the object will never be cached (unless it already is).
 func summonObject(object: String, cache = true):
-    # Check if object exists via ResourceLoader, because when game is exported, objects will be imported into game and can't be checked.
-    if object in objectCache.keys(): # Load the preloaded object instead to prevent major disk usage.
-        var newobj = objectCache[object].instantiate()
-        get_tree().current_scene.add_child(newobj)
-        return newobj
-    else: # Load the object from disk and cache it.
-        if not ResourceLoader.exists(projectConfig.objectsFolder + "/"+ object +".tscn"): push_error("Cannot summon object \"{0}\", file path \"{1}\" could not be found.".format([object, projectConfig.objectsFolder + "/"+ object +".tscn"])); return null;
-        var newobj = load(projectConfig.objectsFolder + "/"+ object +".tscn")
-        if cache:
-            objectCache[object] = newobj
-        newobj = newobj.instantiate()
-        get_tree().current_scene.add_child(newobj)
-        return newobj
+	# Check if object exists via ResourceLoader, because when game is exported, objects will be imported into game and can't be checked.
+	if object in objectCache.keys(): # Load the preloaded object instead to prevent major disk usage.
+		var newobj = objectCache[object].instantiate()
+		get_tree().current_scene.add_child(newobj)
+		return newobj
+	else: # Load the object from disk and cache it.
+		if not ResourceLoader.exists(projectConfig.objectsFolder + "/"+ object +".tscn"): push_error("Cannot summon object \"{0}\", file path \"{1}\" could not be found.".format([object, projectConfig.objectsFolder + "/"+ object +".tscn"])); return null;
+		var newobj = load(projectConfig.objectsFolder + "/"+ object +".tscn")
+		if cache:
+			objectCache[object] = newobj
+		newobj = newobj.instantiate()
+		get_tree().current_scene.add_child(newobj)
+		return newobj
+
+## Get a random tile from the TileMap in the current scene. TileMap must have a unique name (%TileMap).
+func getRandomTilePosOnTilemap(tileAtlasCoords: Vector2i):
+	var tilemap: TileMap = get_tree().current_scene.get_node_or_null("%tileMap")
+	var authorizedTiles = []
+	var nonAuthorizedTiles = []
+	if !tilemap: printerr("USAPI: Could not getRandomTilePosOnTilemap(), no tilemap found. Is it set to a unique name?"); return;
+	for coord in tilemap.get_used_cells(0):
+		if !(tilemap.get_cell_atlas_coords(0, coord) == tileAtlasCoords): nonAuthorizedTiles.append(coord); continue;
+		else: authorizedTiles.append(coord);
+	for coord in nonAuthorizedTiles: if coord in authorizedTiles:
+		authorizedTiles.erase(coord)
+	
+	if authorizedTiles.is_empty(): return;
+
+	return tilemap.to_global(tilemap.map_to_local(authorizedTiles.pick_random()))
+
+		
 
 ## Clears all caches. May free up memory at the cost of lag when loading objects again.
-func clearCache():
-    objectCache.clear()
-    pass
+func clearCaches(type=""):
+	match type:
+		"object":
+			objectCache.clear()
+		"audio":
+			audioCache.clear()
+		"resource":
+			resourceCache.clear()
+		"room":
+			roomCache.clear()
+		_:
+			objectCache.clear()
+			audioCache.clear()
+			resourceCache.clear()
+			roomCache.clear()
